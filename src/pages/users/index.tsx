0a79910ee52968ab3f90e664cb988a4fa3a0e5ed
Jamil Zazu <jamillzazu@gmail.com>
2021-06-11 17:29:44 -0400

    feat: Página: Form de usuário

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,6 +17,7 @@ import React from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from '../../components/Header'
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
@@ -134,6 +135,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex >
     </Box >
