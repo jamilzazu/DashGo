1004b944c22211cdd87904e5b66f5dd7b54a42a4
Jamil Zazu <jamillzazu@gmail.com>
2021-06-11 19:21:32 -0400

    feat: Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,21 @@
 
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
 
-export default function Profile() {
-  return (
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
+export default function Profile({ showProfileData = true }: ProfileProps) {
+  return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Jamil Zazu</Text>
-        <Text color="gray.300" fontSize="small">
-          jamilzazu@hotmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Jamil Zazu</Text>
+          <Text color="gray.300" fontSize="small">
+            jamilzazu@hotmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Jamil Zazu" />
     </Flex>
   );
